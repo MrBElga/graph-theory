b111edab238998c5066bd3548f5515787c9bd30e
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2024-08-17 15:58:58 -0300

    fix: listStructure

--- src/main/java/com/example/grafo/lista/ListaAdj.java
+++
@@ -1,4 +0,0 @@
-package com.example.grafo.lista;
-
-public class ListaAdj {
-}
--- src/main/java/com/example/grafo/matriz/Matriz.java
+++ src/main/java/com/example/grafo/matriz/Matriz.java
@@ -16,23 +16,18 @@ public class Matriz {
 
     public static boolean grafoOrientado(int[][] matrizAdjacencia) {
         int tamanho = matrizAdjacencia.length;
-        for (int i = 0; i < tamanho; i++) {
-            for (int j = 0; j < tamanho; j++) {
-                if (matrizAdjacencia[i][j] != matrizAdjacencia[j][i]) {
+        for (int i = 0; i < tamanho; i++)
+            for (int j = 0; j < tamanho; j++)
+                if (matrizAdjacencia[i][j] != matrizAdjacencia[j][i])
                     return true;
-                }
-            }
-        }
         return false;
     }
 
     public static boolean grafoSimples(int[][] matrizAdjacencia) {
         int tamanho = matrizAdjacencia.length;
-        for (int i = 0; i < tamanho; i++) {
-            if (matrizAdjacencia[i][i] != 0) {
+        for (int i = 0; i < tamanho; i++)
+            if (matrizAdjacencia[i][i] != 0)
                 return false; // Grafo não é simples se houver laços
-            }
-        }
         return true; // É simples
     }
 
@@ -41,14 +36,12 @@ public class Matriz {
         int grau = -1;
         for (int i = 0; i < tamanho; i++) {
             int grauAtual = 0;
-            for (int j = 0; j < tamanho; j++) {
+            for (int j = 0; j < tamanho; j++)
                 grauAtual += matrizAdjacencia[i][j];
-            }
-            if (grau == -1) {
+            if (grau == -1)
                 grau = grauAtual;
-            } else if (grau != grauAtual) {
+             else if (grau != grauAtual)
                 return false; // Não é regular se houver diferentes graus
-            }
         }
         return true; // É regular
     }
