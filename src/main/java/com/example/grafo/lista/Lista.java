690fac120823cbc06c5c8195b02a17268a5c3e9a
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2024-08-17 16:00:05 -0300

    Update: lista.java

--- src/main/java/com/example/grafo/lista/Lista.java
+++ src/main/java/com/example/grafo/lista/Lista.java
@@ -1,5 +1,16 @@
 package com.example.grafo.lista;
 
 public class Lista {
+    private No inicio;
+    private No fim;
 
+    public Lista() {
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+    
+    public void inicializa(){
+        inicio = null;
+        fim = null;
+    }
 }
