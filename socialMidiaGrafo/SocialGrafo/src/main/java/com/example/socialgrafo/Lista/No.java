80e6e1eb4a9a15533b089c7e43b01c0a15d8dad6
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2024-08-31 16:42:02 -0300

    Add: Class Lista and No

---
+++ socialMidiaGrafo/SocialGrafo/src/main/java/com/example/socialgrafo/Lista/No.java
@@ -0,0 +1,50 @@
+package com.example.socialgrafo.Lista;
+
+public class No {
+    public String Aresta;
+    public int Custo;
+    public No prox;
+    public No ant;
+
+    public No() {
+    }
+
+    public No(String aresta, int custo, No prox,No ant) {
+        this.Aresta = aresta;
+        this.Custo = custo;
+        this.ant = ant;
+        this.prox = prox;
+    }
+
+    public String getAresta() {
+        return Aresta;
+    }
+
+    public void setAresta(String aresta) {
+        Aresta = aresta;
+    }
+
+    public int getCusto() {
+        return Custo;
+    }
+
+    public void setCusto(int custo) {
+        Custo = custo;
+    }
+
+    public No getProx() {
+        return prox;
+    }
+
+    public void setProx(No prox) {
+        this.prox = prox;
+    }
+
+    public No getAnt() {
+        return ant;
+    }
+
+    public void setAnt(No ant) {
+        this.ant = ant;
+    }
+}
